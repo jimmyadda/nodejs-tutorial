62ab64c9019b0fc2b3b32b711586ad73ce352db9
Jimmy Adda <ajimmy@technion.ac.il>
2024-02-14 20:32:46 +0200

    product model

--- models/productModel.js
+++ models/productModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     name: {
         type: String,
-        required :[true,"Pleaseinsert product name"]
+        required :[true,"Please insert product name"]
     },
     quantity: {
         type: Number,
